da93ee9218c2bc860606f22f82ca69859c88a6cd
Abhishek pratap Singh <abhishekpratapsingh@Abhisheks-MacBook-Pro.local>
2022-08-29 18:06:34 +0530

    creating form and posting the data in database using post request

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,20 @@ import {
   BrowserRouter,
   Routes,
   Route,
+  Link
 } from "react-router-dom";
 import Home from './pages/Home';
+import CreatePost from './pages/CreatePost';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Link to="/createpost">Create Post</Link>
+      <Link to="/">Home</Link>
         <Routes>
           <Route path="/" exact element={<Home />}/>
+          <Route path="/createpost" exact element={<CreatePost />}/>
         </Routes>
       </BrowserRouter>
     </div>
