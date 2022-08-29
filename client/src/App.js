ff9c55d74503a89d4bac034c40037ff8ee336a17
Abhishek pratap Singh <abhishekpratapsingh@Abhisheks-MacBook-Pro.local>
2022-08-29 10:05:40 +0530

    create client folder for fontend

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
