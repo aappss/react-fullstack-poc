ef1efb102146f951867d57ab41d038e76d4d9bbc
Abhishek pratap Singh <abhishekpratapsingh@Abhisheks-MacBook-Pro.local>
2022-08-29 18:59:19 +0530

    putting comments on file

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,3 +1,4 @@
+// this file contain post api where we can write the feild name which is same as data base feild name 
 module.exports = (sequelize, DataTypes) => {
     const Posts = sequelize.define("Posts", {
         title: {
