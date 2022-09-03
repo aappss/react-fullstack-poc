1721471b90e4a9800cbdeb805a5ae39bd471cbd8
Abhishek pratap Singh <abhishekpratapsingh@Abhisheks-MacBook-Pro.local>
2022-09-03 11:06:50 +0530

    Creating comments table in database and creates router for the table and design comments UI inside post.js file and add fetch and add the comments from comments table from database

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -5,12 +5,30 @@ import axios from 'axios';
 function Post() {
     let { id } = useParams();
     const [postObject, setPostObject] = useState({});
+    const [comments, setComments] = useState([]);
+    const [newComment, setNewComment] = useState("");
 
     useEffect(() => {
         axios.get(`http://localhost:8000/posts/byId/${id}`).then((res) => {
             setPostObject(res.data);
-        })
+        });
+
+        axios.get(`http://localhost:8000/comments/${id}`).then((res) => {
+            setComments(res.data);
+        });
     }, []);
+
+    const addComment = () => {
+        axios.post("http://localhost:8000/comments", {
+            commentBody: newComment,
+            PostId: id
+        }).then((res) => {
+            const commentToAdd = { commentBody: newComment };
+            setComments([...comments, commentToAdd]);
+            setNewComment("");
+        });
+    }
+
     return (
         <div className="postPage">
             <div className="leftSide">
@@ -20,7 +38,21 @@ function Post() {
                     <div className="footer"> {postObject.username}</div>
                 </div>
             </div>
-            <div className="rightSide">Comment Section </div>
+            <div className="rightSide">
+                <div className="addCommentContainer">
+                    <input type="text" placeholder="Comment...." onChange={(e) => (setNewComment(e.target.value))} />
+                    <button onClick={addComment}>Add Comment</button>
+                </div>
+                <div className="listOfComments">
+                    {
+                        comments.map((item, key) => {
+                            return (
+                                <div className="comment" key={key}>{item.commentBody}</div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
         </div>
     )
 }
